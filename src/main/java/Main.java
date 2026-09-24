47b8ecaf45ddb2ca17bd01b35c5208871271dc22
rhuayllag <rhuayllag@unsa.edu.pe>
2026-09-23 21:32:13 -0500

    Convertir a app Spring Boot Web

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,18 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@RestController
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("=================================================");
-        System.out.println("    ¡BIENVENIDO A TU APLICACIÓN CI/CD JAVA!    ");
-        System.out.println("=================================================");
+        SpringApplication.run(Main.class, args);
+    }
 
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        System.out.println("Fecha y Hora de Ejecución: " + dtf.format(LocalDateTime.now()));
-        System.out.println("Entorno de Ejecución: Jenkins Agent");
-        System.out.println("-------------------------------------------------");
-
-        String[] tecnologias = {"Java 17", "Apache Maven", "Jenkins Pipeline", "SonarQube", "Git & GitHub"};
-        System.out.println("Stack del Proyecto:");
-        for (int i = 0; i < tecnologias.length; i++) {
-            System.out.println("  [" + (i + 1) + "] " + tecnologias[i]);
-        }
-
-        System.out.println("=================================================");
-        System.out.println(" ¡Pipeline CI/CD ejecutado y verificado con éxito!");
-        System.out.println("=================================================");
+    @GetMapping("/")
+    public String home() {
+        return "<h1>¡Hola desde Jenkins y Spring Boot!</h1><p>Aplicacion corriendo en el puerto 8083</p>";
     }
 }
