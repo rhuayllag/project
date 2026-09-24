fa08b1dac0c870df40d5e5a2c2af6370aad43e8e
rhuayllag <rhuayllag@unsa.edu.pe>
2026-09-23 21:23:00 -0500

    Modificacion5

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("==========================================");
+        System.out.println(" Aplicación ejecutada con éxito desde Jenkins! ");
+        System.out.println("==========================================");
+    }
+}
